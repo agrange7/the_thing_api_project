75df955865184976e6705de4450e0e5374779347
agrange7 <alexgrange7@gmail.com>
2025-01-20 17:43:50 -0300

    Fix userController

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -15,7 +15,7 @@ export const createUser = async (req, res) => {
     }
 
     await userData.save();
-    res.status(201).json({ message: "User created" });
+    res.status(201).json({ message: "User created", userData });
   } catch (error) {
     return res.status(500).json({ message: "Internal server error", error });
   }
@@ -51,13 +51,17 @@ export const updateUser = async (req, res) => {
 
 export const deleteUser = async (req, res) => {
   try {
-    const _id = req.params._id;
+    const _id = req.params.id;
+    console.log("soy pepe " + _id);
     const userExist = await User.findOne({ _id });
     if (!userExist) {
+      console.log("soy el usuario pepito " + userExist);
       return res.status(404).json({ message: "User not found" });
     }
     await User.findOneAndDelete({ _id });
-    return res.status(200).json({ message: "User deleted succesfully" });
+    return res
+      .status(200)
+      .json({ message: "User deleted succesfully", userExist });
   } catch (error) {
     return res.status(500).json({ error: "Internal server error", error });
   }
